48bee106c5833a2185088ffa7c1e5e63d14b2829
Lexiana <schaller.lexi@gmail.com>
2024-08-01 18:27:12 +0200

    fix:ip request

--- index.js
+++ index.js
@@ -20,19 +20,11 @@ app.get('/', function (req, res) {
 });
 
 // your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.get('/api/whoami', function(req, res){
-  let ip = req.ip ;
- 
-  const language =  req.headers['accept-language'] ;
-  const software = req.headers['user-agent'] ;
   res.json({
-    ipaddress: ip,
-    language: language,
-    software: software
+    ipaddress: req.socket.remoteAddress ,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
   })
 });
 
